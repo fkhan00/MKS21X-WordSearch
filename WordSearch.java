bc8c834ffe795dc96ed4ca92ce137a45cc0e90bd
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2018-11-18 09:30:51 -0500

    added my driver to WordSearch

--- WordSearch.java
+++ WordSearch.java
@@ -45,7 +45,9 @@ public class WordSearch{
           data[i][j] = '_';
         }
       }
-      randgen = new Random();
+      Random seeder = new Random();
+      seed = Math.abs(seeder.nextInt() % 10000);
+      randgen = new Random(seed);
       addAllWords();
       addRandoms();
     }
@@ -208,4 +210,36 @@ public class WordSearch{
       }
       return output;
     }
+  public static void main(String[] args)
+  {
+    try
+    {
+    if(args.length < 3 || args.length > 5)
+    {
+      throw new IndexOutOfBoundsException("\nArguments should be given number of rows, number of columns, \n, document file, and optionally seed number");
+    }
+    if(args.length == 3)
+    {
+    WordSearch testCase2 = new WordSearch(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2]);
+    System.out.println(testCase2.toString());
+  }
+  if (args.length == 4)
+  {
+    WordSearch testCase3 = new WordSearch(Integer.parseInt(args[0]),
+     Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]), "");
+    System.out.println(testCase3.toString());
+  }
+  if (args.length == 5)
+  {
+    WordSearch testCase3 = new WordSearch(Integer.parseInt(args[0]),
+     Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]), args[4]);
+    System.out.println(testCase3.toString());
+  }
+  }
+  catch (IllegalArgumentException error)
+  {
+    System.out.println("You gave a negative input for either row or column");
+  }
+}
+
 }
